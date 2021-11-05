584ea21bc579507ab034116219fc1e95281b7a5c
Jose Pablo Castro <jp@gobu.io>
2021-11-05 13:33:06 -0600

    Se trata de usar una implementación más reciente

--- lib/bamboo/adapters/ses_adapter.ex
+++ lib/bamboo/adapters/ses_adapter.ex
@@ -30,7 +30,7 @@ defmodule Bamboo.SesAdapter do
          |> Mail.put_to(prepare_addresses(email.to))
          |> Mail.put_cc(prepare_addresses(email.cc))
          |> Mail.put_bcc(prepare_addresses(email.bcc))
-         |> Mail.put_subject(email.subject)
+         |> Mail.put_subject(maybe_rfc1342_encode(email.subject))
          |> put_headers(email.headers)
          |> put_text(email.text_body)
          |> put_html(email.html_body)
@@ -70,20 +70,39 @@ defmodule Bamboo.SesAdapter do
     Enum.reduce(
       attachments,
       message,
-      &Mail.put_attachment(&2, {&1.filename, &1.data})
+      fn attachment, message ->
+        headers =
+          if attachment.content_id do
+            [content_id: attachment.content_id]
+          else
+            []
+          end
+
+        opts = [headers: headers]
+
+        Mail.put_attachment(message, {attachment.filename, attachment.data}, opts)
+      end
     )
   end
 
   defp put_text(message, nil), do: message
 
   defp put_text(message, body) do
-    Mail.put_text(message, body, charset: "UTF-8")
+    if ascii?(body) do
+      Mail.put_text(message, body)
+    else
+      Mail.put_text(message, body, charset: "UTF-8")
+    end
   end
 
   defp put_html(message, nil), do: message
 
   defp put_html(message, body) do
-    Mail.put_html(message, body, charset: "UTF-8")
+    if ascii?(body) do
+      Mail.put_html(message, body)
+    else
+      Mail.put_html(message, body, charset: "UTF-8")
+    end
   end
 
   @doc """
@@ -92,37 +111,76 @@ defmodule Bamboo.SesAdapter do
   def set_configuration_set(mail, configuration_set_name),
     do: Bamboo.Email.put_private(mail, :configuration_set_name, configuration_set_name)
 
-  @doc """
-  Set the SES template
-  """
-  def set_template(mail, template),
-    do: Bamboo.Email.put_private(mail, :template, template)
-
-  @doc """
-  Set the SES template data
-  """
-  def set_template_data(mail, template_data) when is_map(template_data) do
-    Bamboo.Email.put_private(mail, :template_data, Jason.encode!(template_data))
-  end
-
   defp prepare_addresses(recipients), do: Enum.map(recipients, &prepare_address(&1))
 
   defp prepare_address({nil, address}), do: encode_address(address)
   defp prepare_address({"", address}), do: encode_address(address)
 
-  defp prepare_address({name, address}),
-    do: {q_encode(name), encode_address(address)}
-
-  defp q_encode(string) when is_binary(string) do
-    q_encoded = String.replace(Mail.Encoders.QuotedPrintable.encode(string), "=\r\n", "")
-
-    "=?utf-8?Q?#{q_encoded}?="
+  defp prepare_address({name, address}) do
+    {maybe_rfc1342_encode(name), encode_address(address)}
   end
-
-  defp q_encode(string), do: string
 
   defp encode_address(address) do
     [local_part, domain_part] = String.split(address, "@")
     Enum.join([Mail.Encoders.SevenBit.encode(local_part), :idna.utf8_to_ascii(domain_part)], "@")
+  end
+
+  defp maybe_rfc1342_encode(string) when is_binary(string) do
+    should_encode? = !ascii?(string) || String.contains?(string, ["\"", "?"])
+
+    if should_encode? do
+      rfc1342_encode(string)
+    else
+      string
+    end
+  end
+
+  defp maybe_rfc1342_encode(_), do: nil
+
+  defp rfc1342_encode(string) when is_binary(string) do
+    rfc1342_encode(string, [])
+  end
+
+  defp rfc1342_encode(_), do: nil
+
+  def rfc1342_encode("", acc), do: acc |> Enum.reverse() |> Enum.join(" ")
+
+  def rfc1342_encode(string, acc) do
+    # https://tools.ietf.org/html/rfc1342
+    # > An encoded-word may not be more than 75 characters long, including
+    # > charset, encoding, encoded-text, and delimiters.  If it is desirable
+    # > to encode more text than will fit in an encoded-word of 75
+    # > characters, multiple encoded-words (separated by SPACE or newline)
+    # > may be used.
+    maximum_possible_text_length =
+      rfc1342_maximum_encoded_word_length() - String.length(encode_word(""))
+
+    {encoded, rest} =
+      maximum_possible_text_length..1
+      |> Enum.reduce_while(nil, fn n, _ ->
+        {word, rest} = String.split_at(string, n)
+        encoded = encode_word(word)
+
+        if String.length(encoded) <= rfc1342_maximum_encoded_word_length() do
+          {:halt, {encoded, rest}}
+        else
+          {:cont, nil}
+        end
+      end)
+
+    rfc1342_encode(rest, [encoded | acc])
+  end
+
+  defp ascii?(string) do
+    non_ascii_chars = Enum.uniq(String.codepoints(string)) -- Enum.map(0..127, fn x -> <<x>> end)
+    Enum.empty?(non_ascii_chars)
+  end
+
+  defp rfc1342_maximum_encoded_word_length do
+    75
+  end
+
+  defp encode_word(word) do
+    "=?utf-8?B?#{Base.encode64(word)}?="
   end
 end
